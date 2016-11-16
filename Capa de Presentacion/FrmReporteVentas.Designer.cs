8830e7e4af7dd8029d8595fb3ac0e69d9bd9181d
LAngelPillco <u2013227854@continental.edu.pe>
2016-11-16 16:50:15 -0500

    Capa de presentación
    En esta capa se encuentran todos los formularios que serán con lo que se dará uso al Sistema. Actualmente, el sistema, cuenta con formularios para lo siguiente:
    - Login.
    - Listado de cargos
    - Listado de categorías.
    - Listado de Clientes.
    - Listado de Empleados.
    - Listado de Productos.
    - Registro de Cargo.
    - Registro de Categoría.
    - Registro de Empleados.
    - Registro de Usuarios.
    - Registro de Usuarios.
    - Registro de Clientes.
    - Registro de Productos.
    - Registro de ventas

---
+++ Capa de Presentacion/FrmReporteVentas.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Capa_de_Presentacion
+{
+    partial class FrmReporteVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.costo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nombre,
+            this.apellidos,
+            this.serie,
+            this.numero,
+            this.tipo,
+            this.fecha,
+            this.costo});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(868, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Cliente nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // apellidos
+            // 
+            this.apellidos.HeaderText = "Cliente apellidos";
+            this.apellidos.Name = "apellidos";
+            this.apellidos.ReadOnly = true;
+            // 
+            // serie
+            // 
+            this.serie.HeaderText = "Serie";
+            this.serie.Name = "serie";
+            this.serie.ReadOnly = true;
+            // 
+            // numero
+            // 
+            this.numero.HeaderText = "Numero de documento";
+            this.numero.Name = "numero";
+            this.numero.ReadOnly = true;
+            // 
+            // tipo
+            // 
+            this.tipo.HeaderText = "Tipo de documento";
+            this.tipo.Name = "tipo";
+            this.tipo.ReadOnly = true;
+            // 
+            // fecha
+            // 
+            this.fecha.HeaderText = "Fecha";
+            this.fecha.Name = "fecha";
+            this.fecha.ReadOnly = true;
+            // 
+            // costo
+            // 
+            this.costo.HeaderText = "Costo";
+            this.costo.Name = "costo";
+            this.costo.ReadOnly = true;
+            // 
+            // FrmReporteVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 453);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.Name = "FrmReporteVentas";
+            this.Text = "FrmReporteVentas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn serie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn costo;
+    }
+}
---
+++ Capa de Presentacion/FrmReporteVentas.cs
@@ -0,0 +1,54 @@
+﻿using CapaLogicaNegocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Capa_de_Presentacion
+{
+    public partial class FrmReporteVentas : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        private clsVentas P = new clsVentas();
+        
+
+        public FrmReporteVentas()
+        {
+            InitializeComponent();
+        }
+        private void FrmVentas_Load(object sender, EventArgs e)
+        {
+            CargarListado();
+            dataGridView1.ClearSelection();
+        }
+        private void CargarListado()
+        {
+            DataTable dt = new DataTable();
+            dt = P.Listar();
+            try
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    dataGridView1.Rows.Add(dt.Rows[i][0]);
+                    dataGridView1.Rows[i].Cells[0].Value = dt.Rows[i][0].ToString();
+                    dataGridView1.Rows[i].Cells[1].Value = dt.Rows[i][1].ToString();
+                    dataGridView1.Rows[i].Cells[2].Value = dt.Rows[i][2].ToString();
+                    dataGridView1.Rows[i].Cells[3].Value = dt.Rows[i][3].ToString();
+                    dataGridView1.Rows[i].Cells[4].Value = dt.Rows[i][4].ToString();
+                    dataGridView1.Rows[i].Cells[5].Value = dt.Rows[i][5].ToString();
+                    dataGridView1.Rows[i].Cells[6].Value = Convert.ToDateTime(dt.Rows[i][6].ToString()).ToShortDateString();
+                    dataGridView1.Rows[i].Cells[7].Value = dt.Rows[i][7].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dataGridView1.ClearSelection();
+        }
+    }
+}
